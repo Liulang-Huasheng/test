69b70fcc9aaff1205d0ba5f7fba8ac0761aeaccb
Cookie-97 <940090764@qq.com>
2019-04-25 21:19:11 +0800

    commit 1

---
+++ src/main/java/com/tan/a/User.java
@@ -0,0 +1,12 @@
+package com.tan.a;
+
+/**
+ * @Author: 流浪花生-Tan
+ * @CreateTime: 2019-04-25 20:38
+ * @Description:
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("-------------------11111111");
+    }
+}
---
+++ src/main/java/com/tan/b/User2.java
@@ -0,0 +1,12 @@
+package com.tan.b;
+
+/**
+ * @Author: 流浪花生-Tan
+ * @CreateTime: 2019-04-25 20:38
+ * @Description:
+ */
+public class User2 {
+    public static void main(String[] args) {
+        System.out.println("-------------------222222222222");
+    }
+}
